5d6d2d2b666fa6a7039d7751a232b6b1ca4c0be0
Henry Zhang <hzhang@freelancer.com>
2024-01-15 17:18:11 +1100

    Angular 17 repro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule, RouterOutlet],
   template: `
     <div>No form control</div>
     <input type="text" />
@@ -13,7 +17,7 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'hydration-form-repro';
+  title = 'hydration-form-repro-17';
 
   control = new FormControl();
 }
