d0b4223ce616307932fff2a0b6c02f48b7bc473c
Henry Zhang <hzhang@freelancer.com>
2024-05-30 15:18:19 +1000

    Rebuild in 18

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,16 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import {
+  provideClientHydration,
+  withI18nSupport,
+} from '@angular/platform-browser';
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withI18nSupport()),
+  ],
 };
