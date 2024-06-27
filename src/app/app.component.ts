1886468b6bc379f851bf6f4a5754d891b0b0fce3
Henry Zhang <hzhang@freelancer.com>
2024-06-27 16:30:59 +1000

    New issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,22 +3,73 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-text',
-  template: '<ng-content></ng-content>',
+  selector: 'app-dropdown-content',
+  template: `<ng-content></ng-content>`,
   standalone: true,
 })
-export class TextComponent {}
+export class DropdownContentComponent {}
+
+@Component({
+  selector: 'app-dropdown-trigger',
+  template: `<ng-content></ng-content>`,
+  standalone: true,
+})
+export class DropdownTriggerComponent {}
+
+@Component({
+  selector: 'app-dropdown',
+  template: `
+    <ng-content select="app-dropdown-trigger"></ng-content>
+    <!-- dynamically display this using @if or cdkPortal or something else -->
+    <ng-template>
+      <ng-content select="app-dropdown-content"></ng-content>
+    </ng-template>
+  `,
+  standalone: true,
+})
+export class DropdownComponent {
+  isOpen = false;
+}
+
+@Component({
+  selector: 'app-extras-menu-item',
+  template: `<ng-content></ng-content>`,
+  standalone: true,
+})
+export class ExtrasMenuItemComponent {}
+
+@Component({
+  imports: [
+    DropdownComponent,
+    DropdownContentComponent,
+    DropdownTriggerComponent,
+  ],
+  selector: 'app-extras-menu',
+  template: `<app-dropdown>
+    <app-dropdown-trigger>More</app-dropdown-trigger>
+    <app-dropdown-content>
+      <ng-content select="app-extras-menu-item"></ng-content>
+    </app-dropdown-content>
+  </app-dropdown>`,
+  standalone: true,
+})
+export class ExtrasMenuComponent {}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, TextComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    ExtrasMenuComponent,
+    ExtrasMenuItemComponent,
+  ],
   template: `
-    <app-text i18n>
-      Before
-      <br />
-      After
-    </app-text>
+    <app-extras-menu>
+      <app-extras-menu-item>Menu Item 1</app-extras-menu-item>
+      <app-extras-menu-item>Menu Item 2</app-extras-menu-item>
+      <app-extras-menu-item>Menu Item 3</app-extras-menu-item>
+    </app-extras-menu>
   `,
   styleUrls: ['./app.component.css'],
 })
